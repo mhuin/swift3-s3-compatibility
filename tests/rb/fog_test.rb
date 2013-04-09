2f8ab68f4437095c31b4ef683eb831e7cd8d04d4
Matthieu Huin <mhu@enovance.com>
2013-04-09 14:20:35 +0200

    test class setup works, but SSL has to be deactivated

--- tests/rb/fog_test.rb
+++ tests/rb/fog_test.rb
@@ -3,28 +3,36 @@
 require 'fog'
 require "test/unit"
 require "./common.rb"
- 
+
+# Do not verify SSL (less secure)
+require 'excon'
+Excon.defaults[:ssl_verify_peer] = false
+
 class TestFog < Test::Unit::TestCase
     # ruby TestCase doesn't have anything similar to setup_class
-    @connection = Fog::Storage.new({
-      :provider                 => 'AWS',
-      :aws_secret_access_key    => ACCESS_KEY,
-      :aws_access_key_id        => SECRET_KEY,
-      :host                     => S3_HOST,
-      :port                     => S3_PORT
-    })
+    @@connection = Fog::Storage.new({
+          :provider                 => 'AWS',
+          :aws_secret_access_key    => SECRET_KEY,
+          :aws_access_key_id        => ACCESS_KEY,
+          :scheme                   => 'http',
+          :host                     => S3_HOST,
+          :port                     => S3_PORT
+        })
     begin
-        bucket = @connection.directories.get("fogs3testing")
+        bucket = @@connection.directories.get("fogs3testing")
         bucket.files.each do |to_discard|
             to_discard.destroy
         end
         bucket.destroy
     rescue
         puts "no cleanup needed, woohoo"
-    end
+    end 
 
     def test_01_create_bucket
-        assert(false, "Not implemented yet")
+        bucket = @@connection.directories.create(
+          :key                      => "fogs3testing",
+          :public                   => true)
+        assert_equal(1, @@connection.directories.size)
     end
 
     def test_02_add_key
